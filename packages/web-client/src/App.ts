c7b8db1cb971055cb17583d2784a988e8aaac562
kanatamon <ponde.moonlord@gmail.com>
2022-07-26 16:06:46 +0700

    init: setup mono-repo

---
+++ packages/web-client/src/App.ts
@@ -0,0 +1,10 @@
+import './index.css'
+
+document.getElementById('app').innerHTML = `
+<div class="container">
+  <div>Name: web-client</div>
+  <div>Framework: vanilla</div>
+  <div>Language: TypeScript</div>
+  <div>CSS: Empty CSS</div>
+</div>
+`
